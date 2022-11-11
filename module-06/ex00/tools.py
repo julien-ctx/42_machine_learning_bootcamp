d47c0a45a13408d2d28c4a492d4e0aa03923cdec
julien-ctx <jdaodkql@gmail.com>
2022-11-11 19:58:48 +0100

    Gradient calculation OK

---
+++ module-06/ex00/tools.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def add_intercept(x):
+	"""Adds a column of 1's to the non-empty numpy.array x.
+	Args:
+	x: has to be a numpy.array of dimension m * n.
+	Returns:
+	X, a numpy.array of dimension m * (n + 1).
+	None if x is not a numpy.array.
+	None if x is an empty numpy.array.
+	Raises:
+	This function should not raise any Exception.
+	"""
+	if not isinstance(x, (np.ndarray, np.generic)):
+		return None
+	if not x.size:
+		return None
+	if x.ndim == 1:
+		x = x[:,np.newaxis]
+	ones = np.ones((len(x), 1))
+	return np.hstack((ones, x))
+
+if __name__ == "__main__":
+	x = np.arange(1,6)
+	print(add_intercept(x))
+
+	y = np.arange(1,10).reshape((3,3))
+	print(add_intercept(y))
